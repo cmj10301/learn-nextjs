00870b364fd29bd9c125eb1fa2255a2de0fde355
cmj10301 <cmj10301@gmail.com>
2024-10-21 01:27:52 +0900

    3.4까지

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,29 @@
-import { Metadata } from "next"
+import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata :Metadata = {
-  title : "Home",
+  title: "Home",
 }
 
-export default function Page() {
-  return <h1>Hello!</h1>
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+async function getMovies() {
+  await new Promise((reslove) => setTimeout(reslove,2000));
+
+  const response = await fetch(API_URL);
+  const json = await response.json();
+  return json;
+}
+
+export default async function HomePage() {
+  const movies = await getMovies();
+  return (
+    <div>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.id},{movie.title}</Link>
+        </li>
+      ))}
+    </div>
+  );
 }
